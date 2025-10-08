f7a8cd49e49709e1bf1f8c46e61c6aa3bc0c6160
Fabinho <fabiohenr8queluzsantos@gmail.com>
2025-10-07 22:50:08 -0300

    ajustes

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -4,11 +4,13 @@ import { createMenuObject } from '../helpers/createMenuObject.js'
 
 export const search: RequestHandler = (req, res) => {
     let query: string = req.query.q as string;
+    if (!query){
+        return res.redirect('/')
+    }
     console.log(query)
     let list = Pet.getFromName(query)
     res.render('pages/page', {
         menu: createMenuObject(''),
         list,
     });
-    
 } 
